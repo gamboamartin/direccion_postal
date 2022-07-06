6da03bbed013fb3ae502b40cd4b2b0d34d1c1f96
Angel Martínez De León <angelmartinezdeleon@gmail.com>
2022-07-06 14:44:52 -0500

    se agregaron las entidades dp_calle, dp_calle_pertence, dp_colonia, dp_colonia_postal, dp_cp, dp_estado, dp_municipio

---
+++ controllers/controlador_dp_calle.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace controllers;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\links_menu;
+use gamboamartin\system\system;
+use html\dp_calle_html;
+use html\dp_pais_html;
+use models\dp_calle;
+use PDO;
+use stdClass;
+
+class controlador_dp_calle extends system {
+
+    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+        $modelo = new dp_calle(link: $link);
+        $html = new dp_calle_html();
+        $obj_link = new links_menu($this->registro_id);
+        parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $this->titulo_lista = 'Calles';
+
+    }
+
+
+}
---
+++ templates/directivas/dp_calle_html.php
@@ -0,0 +1,10 @@
+<?php
+namespace html;
+
+use gamboamartin\system\html_controler;
+
+
+class dp_calle_html extends html_controler {
+
+
+}
