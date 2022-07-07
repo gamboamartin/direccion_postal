3462d3724a261b5f9dd72c8c35fb2bd9c62c3c28
Angel Martínez De León <angelmartinezdeleon@gmail.com>
2022-07-07 13:32:03 -0500

    se agregó select pais a entidad municipio

--- templates/inputs/dp_municipio/alta.php
+++ templates/inputs/dp_municipio/alta.php
@@ -1,6 +1,7 @@
 <?php /** @var controllers\controlador_dp_estado $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
 <?php echo $controlador->forms_inputs_alta; ?>
+<?php include "templates/selects/dp_pais_id.php"; ?>
 <?php include "templates/selects/dp_estado_id.php"; ?>
 <?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
 <div class="control-group btn-alta">
