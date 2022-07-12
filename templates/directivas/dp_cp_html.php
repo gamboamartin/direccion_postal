a475b943e8909157c7fdf06c8a229379be8834d5
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-12 14:02:37 -0500

    v0.6.0 Se integra colonia_postal la cual liga la calle con el copido postal

--- templates/directivas/dp_cp_html.php
+++ templates/directivas/dp_cp_html.php
@@ -1,10 +1,22 @@
 <?php
 namespace html;
 
+use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use models\dp_cp;
+use PDO;
 
 
 class dp_cp_html extends html_controler {
 
+    public function select_dp_cp_id(int $id_selected, PDO $link): array|string
+    {
+        $modelo = new dp_cp($link);
 
+        $select = $this->select_catalogo(id_selected:$id_selected, modelo: $modelo);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 }
--- templates/directivas/dp_municipio_html.php
+++ templates/directivas/dp_municipio_html.php
@@ -1,10 +1,22 @@
 <?php
 namespace html;
 
+use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use models\dp_municipio;
+use PDO;
 
 
 class dp_municipio_html extends html_controler {
+    public function select_dp_municipio_id(int $id_selected, PDO $link): array|string
+    {
+        $modelo = new dp_municipio($link);
 
+        $select = $this->select_catalogo(id_selected:$id_selected, modelo: $modelo);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 
 }
