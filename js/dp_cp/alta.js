4fe5dff36ab6fd3ed666a511f63c668d5a8ff45e
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-13 11:24:27 -0500

    v0.13.0 Maquetacion de un select de tipo estado para obtener los municipios via get por estado_id

--- js/dp_cp/alta.js
+++ js/dp_cp/alta.js
@@ -6,11 +6,17 @@ function getParameterByName(name) {
 }
 let session_id = getParameterByName('session_id');
 let dp_estado_id = -1;
-$("#dp_estado_id").change(function(){
+let sl_dp_municipio_id = $("#dp_municipio_id");
+let sl_dp_estado_id = $("#dp_estado_id");
+sl_dp_estado_id.change(function(){
     dp_estado_id = $(this).val();
     let url = "index.php?seccion=dp_municipio&ws=1&accion=get_municipio&dp_estado_id="+dp_estado_id+"&session_id="+session_id;
     $.get(url, function(data, status){
-        console.log(url);
-        console.log(data);
+        $.each(data.registros, function( index, dp_municipio ) {
+            var new_option ="<option value ="+dp_municipio.dp_municipio_id+">"+dp_municipio.dp_estado_descripcion+' '+dp_municipio.dp_municipio_descripcion+"</option>";
+            $(new_option).appendTo("#dp_municipio_id");
+        });
+        sl_dp_municipio_id.selectpicker('refresh');
+
     });
 });
