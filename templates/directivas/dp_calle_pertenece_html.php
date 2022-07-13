d6f0fc4f950b7256f6ba5c2228503d2a82c2a593
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-13 17:01:04 -0500

    v0.17.3 Se genera template select calñle_pertenece en html

--- templates/directivas/dp_calle_pertenece_html.php
+++ templates/directivas/dp_calle_pertenece_html.php
@@ -1,10 +1,22 @@
 <?php
 namespace html;
 
+use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use models\dp_calle_pertenece;
+use PDO;
 
 
 class dp_calle_pertenece_html extends html_controler {
 
+    public function select_dp_calle_pertenece_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    {
+        $modelo = new dp_calle_pertenece($link);
 
+        $select = $this->select_catalogo(cols:$cols, con_registros: $con_registros,id_selected:$id_selected, modelo: $modelo);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 }
