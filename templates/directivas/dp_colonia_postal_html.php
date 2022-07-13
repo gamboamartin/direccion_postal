cbb4fc2ff5b07f960070ccb6a53e6ecf175ef1a5
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-13 15:46:54 -0500

    v0.15.3 FIX se integra colonia postal para filtrar cp y colonia

--- templates/directivas/dp_colonia_postal_html.php
+++ templates/directivas/dp_colonia_postal_html.php
@@ -1,10 +1,22 @@
 <?php
 namespace html;
 
+use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use models\dp_colonia_postal;
+use PDO;
 
 
 class dp_colonia_postal_html extends html_controler {
+    public function select_dp_colonia_postal_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    {
+        $modelo = new dp_colonia_postal($link);
 
+        $select = $this->select_catalogo(cols:$cols, con_registros: $con_registros,id_selected:$id_selected, modelo: $modelo);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 
 }
