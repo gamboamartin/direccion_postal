f1878f1858da76c7e5b439b55a4d9f2a8b9fd5cd
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-14 13:02:30 -0500

    v0.18.4 Se integran relaciones nuevas a calle_pertenece donde relaciona desde cp hasta pais y a colonia

--- orm/dp_calle_pertenece.php
+++ orm/dp_calle_pertenece.php
@@ -6,7 +6,8 @@ use PDO;
 class dp_calle_pertenece extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla=>false,'dp_colonia_postal'=>$tabla,'dp_calle'=>$tabla,'dp_cp'=>'dp_colonia_postal',
+            'dp_colonia'=>'dp_colonia_postal','dp_municipio'=>'dp_cp','dp_estado'=>'dp_municipio','dp_pais'=>'dp_estado');
         $campos_obligatorios[] = 'descripcion';
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
