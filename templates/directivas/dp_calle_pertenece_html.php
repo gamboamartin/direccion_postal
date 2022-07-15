9e130fc7e795c3462d6d35e84133d20d494eb792
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-15 10:05:20 -0500

    v0.21.5 Se generan inputs entre calles para carga de empresas

--- templates/directivas/dp_calle_pertenece_html.php
+++ templates/directivas/dp_calle_pertenece_html.php
@@ -19,4 +19,30 @@ class dp_calle_pertenece_html extends html_controler {
         }
         return $select;
     }
+
+    public function select_dp_calle_pertenece_entre1_id(int $cols, bool $con_registros, int $id_selected,
+                                                        PDO $link): array|string
+    {
+        $modelo = new dp_calle_pertenece($link);
+
+        $select = $this->select_catalogo(cols:$cols, con_registros: $con_registros,id_selected:$id_selected,
+            modelo: $modelo, label: 'Entre Calle');
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
+
+    public function select_dp_calle_pertenece_entre2_id(int $cols, bool $con_registros, int $id_selected,
+                                                        PDO $link): array|string
+    {
+        $modelo = new dp_calle_pertenece($link);
+
+        $select = $this->select_catalogo(cols:$cols, con_registros: $con_registros,id_selected:$id_selected,
+            modelo: $modelo, label: 'Entre Calle');
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 }
