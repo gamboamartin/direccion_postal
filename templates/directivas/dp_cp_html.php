55af5c49b721722647bdf80b7d125f19183d6219
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-03 17:18:43 -0500

    v0.60.7 Se genera ut para select cp

--- templates/directivas/dp_cp_html.php
+++ templates/directivas/dp_cp_html.php
@@ -3,6 +3,7 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use gamboamartin\template_1\directivas;
 use models\dp_cp;
 use PDO;
 use stdClass;
@@ -10,9 +11,27 @@ use stdClass;
 
 class dp_cp_html extends html_controler {
 
+    /**
+     * Genera un select de tipo cp
+     * @param int $cols Numero de columnas en css
+     * @param bool $con_registros si no con registros deja un select vacio
+     * @param int $id_selected Id seleccionado
+     * @param PDO $link conexion a la base de datos
+     * @param array $filtro Filtro para obtencion de datos
+     * @return array|string
+     * @version 0.60.8
+     * @verfuncion 0.1.0
+     * @fecha 2022-08-03 17:15
+     * @author mgamboa
+     */
     public function select_dp_cp_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
                                     array $filtro = array()): array|string
     {
+
+        $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
+        }
         $modelo = new dp_cp($link);
 
         $select = $this->select_catalogo(cols: $cols, con_registros: $con_registros, id_selected: $id_selected,
--- tests/templates/directivas/dp_cp_htmlTest.php
+++ tests/templates/directivas/dp_cp_htmlTest.php
@@ -4,11 +4,12 @@ namespace tests\links\secciones;
 use gamboamartin\errores\errores;
 use gamboamartin\template_1\html;
 use gamboamartin\test\test;
+use html\dp_cp_html;
 use html\dp_estado_html;
 use stdClass;
 
 
-class dp_estado_htmlTest extends test {
+class dp_cp_htmlTest extends test {
     public errores $errores;
     private stdClass $paths_conf;
     public function __construct(?string $name = null, array $data = [], $dataName = '')
@@ -23,32 +24,33 @@ class dp_estado_htmlTest extends test {
 
     /**
      */
-    public function test_select_dp_estado_id(): void
+    public function test_select_dp_cp_id(): void
     {
         errores::$error = false;
         $_GET['session_id'] = 1;
         $_GET['seccion'] = 'dp_estado';
         $html = new html();
-        $dir = new dp_estado_html($html);
+        $dir = new dp_cp_html($html);
 
         $cols = 1;
         $con_registros = false;
         $id_selected = -1;
-        $resultado = $dir->select_dp_estado_id($cols, $con_registros, $id_selected, $this->link);
+        $resultado = $dir->select_dp_cp_id($cols, $con_registros, $id_selected, $this->link);
 
         $this->assertIsString($resultado);
         $this->assertNotTrue(errores::$error);
-        $this->assertEquals("<div class='control-group col-sm-1'><label class='control-label' for='dp_estado_id'>Estado</label><div class='controls'><select class='form-control selectpicker color-secondary dp_estado_id' id='dp_estado_id' name='dp_estado_id' ><option value=''  >Selecciona una opcion</option></select></div></div>",$resultado);
+        $this->assertEquals("<div class='control-group col-sm-1'><label class='control-label' for='dp_cp_id'>CP</label><div class='controls'><select class='form-control selectpicker color-secondary dp_cp_id' id='dp_cp_id' name='dp_cp_id' ><option value=''  >Selecciona una opcion</option></select></div></div>",$resultado);
 
 
         errores::$error = false;
         $cols = 1;
         $con_registros = true;
         $id_selected = -1;
-        $resultado = $dir->select_dp_estado_id($cols, $con_registros, $id_selected, $this->link);
+        $resultado = $dir->select_dp_cp_id($cols, $con_registros, $id_selected, $this->link);
+        errores::$error = false;
         $this->assertIsString($resultado);
         $this->assertNotTrue(errores::$error);
-        $this->assertStringContainsStringIgnoringCase("<div class='control-group col-sm-1'><label class='control-label' for='dp_estado_id'>Estado</label><div class='controls'>",$resultado);
+        $this->assertStringContainsStringIgnoringCase("<div class='control-group col-sm-1'><label class='control-label' for='dp_cp_id'>CP</label><div class='controls'><select class='form-control",$resultado);
 
         errores::$error = false;
     }
