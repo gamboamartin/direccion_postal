82fd91dd3a9f7bdacba48205e77bce6522def88b
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-03 15:11:54 -0500

    v0.57.7 Se integra filtro en municipio

--- templates/directivas/dp_municipio_html.php
+++ templates/directivas/dp_municipio_html.php
@@ -8,12 +8,13 @@ use PDO;
 
 
 class dp_municipio_html extends html_controler {
-    public function select_dp_municipio_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    public function select_dp_municipio_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
+                                           array $filtro = array()): array|string
     {
         $modelo = new dp_municipio($link);
 
-        $select = $this->select_catalogo(cols:$cols, con_registros: $con_registros,id_selected:$id_selected,
-            modelo: $modelo, label: 'Municipio');
+        $select = $this->select_catalogo(cols: $cols, con_registros: $con_registros, id_selected: $id_selected,
+            modelo: $modelo, filtro: $filtro, label: 'Municipio');
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
