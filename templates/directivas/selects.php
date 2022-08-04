8236efedc91f5ad1f628ca8eedc45f234f803066
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-04 17:01:49 -0500

    v0.77.8 Se simplifica estado

--- templates/directivas/selects.php
+++ templates/directivas/selects.php
@@ -16,13 +16,35 @@ class selects {
         $this->error = new errores();
     }
 
+    public function dp_estado_id(html $html, PDO $link, stdClass $row): array|stdClass
+    {
+
+
+        $row = (new init())->row_value_id($row, tabla: 'dp_estado');
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener id',data:  $row);
+        }
+
+        $select = (new dp_estado_html(html: $html))->select_dp_estado_id(cols: 6, con_registros:true,
+            id_selected:$row->dp_estado_id,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+
+        }
+
+        $data = new stdClass();
+        $data->row = $row;
+        $data->select = $select;
+        return $data;
+    }
+
     public function dp_pais_id(html $html, PDO $link, stdClass $row): array|stdClass
     {
 
 
         $row = (new init())->row_value_id($row, tabla: 'dp_pais');
         if(errores::$error){
-            return $this->error->error(mensaje: 'Error al obtener pais_id',data:  $row);
+            return $this->error->error(mensaje: 'Error al obtener id',data:  $row);
         }
 
         $select = (new dp_pais_html(html: $html))->select_dp_pais_id(cols: 6, con_registros:true,
