9eb843ed871bfa395a9c9dbc31f1e078ccdb4f01
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-04 11:53:47 -0500

    v0.65.7 Se inicilaiza doc de calle pertenece

--- templates/directivas/dp_calle_pertenece_html.php
+++ templates/directivas/dp_calle_pertenece_html.php
@@ -3,6 +3,7 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use gamboamartin\template\directivas;
 use models\dp_calle_pertenece;
 use PDO;
 use stdClass;
@@ -34,9 +35,22 @@ class dp_calle_pertenece_html extends html_controler {
         return $div;
     }
 
+    /**
+     * @param int $cols
+     * @param bool $con_registros
+     * @param int $id_selected
+     * @param PDO $link
+     * @param array $filtro
+     * @return array|string
+     */
     public function select_dp_calle_pertenece_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
                                                  array $filtro = array()): array|string
     {
+
+        $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
+        }
         $modelo = new dp_calle_pertenece($link);
 
         $select = $this->select_catalogo(cols: $cols, con_registros: $con_registros, id_selected: $id_selected,
