aa5bac5b929de790b94ff5b30374c77e056bec75
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-05 10:09:10 -0500

    v0.84.8 Se refactorizan selects

--- templates/directivas/selects.php
+++ templates/directivas/selects.php
@@ -16,24 +16,22 @@ class selects {
         $this->error = new errores();
     }
 
-    /**
-     * Genera un select de tipo estado inicializado
-     * @param html $html Clade de template
-     * @param PDO $link conexion a bd
-     * @param stdClass $row Registro en operacion
-     * @return array|stdClass
-     */
-    public function dp_estado_id(html $html, PDO $link, stdClass $row): array|stdClass
+    private function select_base(html $html, PDO $link, stdClass $row, string $tabla): array|stdClass
     {
         $row_ = $row;
 
-        $row_ = (new init())->row_value_id($row_, tabla: 'dp_estado');
+        $row_ = (new init())->row_value_id($row_, tabla: $tabla);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al obtener id',data:  $row_);
         }
 
-        $select = (new dp_estado_html(html: $html))->select_dp_estado_id(cols: 6, con_registros:true,
-            id_selected:$row_->dp_estado_id,link: $link);
+        $name_obj = "html\\".$tabla.'_html';
+        $obj_html = new $name_obj(html: $html);
+        $key_id = $tabla.'_id';
+        $name_function = 'select_'.$key_id;
+
+
+        $select = $obj_html->$name_function(cols: 6, con_registros:true, id_selected:$row_->$key_id,link: $link);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select',data:  $select);
 
@@ -52,25 +50,33 @@ class selects {
      * @param stdClass $row Registro en operacion
      * @return array|stdClass
      */
+    public function dp_estado_id(html $html, PDO $link, stdClass $row): array|stdClass
+    {
+
+        $data = $this->select_base(html: $html,link:  $link, row: $row,tabla:  'dp_estado');
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $data);
+
+        }
+
+        return $data;
+    }
+
+    /**
+     * Genera un select de tipo estado inicializado
+     * @param html $html Clade de template
+     * @param PDO $link conexion a bd
+     * @param stdClass $row Registro en operacion
+     * @return array|stdClass
+     */
     public function dp_municipio_id(html $html, PDO $link, stdClass $row): array|stdClass
     {
-        $row_ = $row;
-
-        $row_ = (new init())->row_value_id($row_, tabla: 'dp_municipio');
+        $data = $this->select_base(html: $html,link:  $link, row: $row,tabla:  'dp_municipio');
         if(errores::$error){
-            return $this->error->error(mensaje: 'Error al obtener id',data:  $row_);
-        }
-
-        $select = (new dp_municipio_html(html: $html))->select_dp_municipio_id(cols: 6, con_registros:true,
-            id_selected:$row_->dp_estado_id,link: $link);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+            return $this->error->error(mensaje: 'Error al generar select',data:  $data);
 
         }
 
-        $data = new stdClass();
-        $data->row = $row_;
-        $data->select = $select;
         return $data;
     }
 
@@ -89,22 +95,12 @@ class selects {
     public function dp_pais_id(html $html, PDO $link, stdClass $row): array|stdClass
     {
 
-        $row_ = $row;
-        $row_ = (new init())->row_value_id($row_, tabla: 'dp_pais');
+        $data = $this->select_base(html: $html,link:  $link, row: $row,tabla:  'dp_pais');
         if(errores::$error){
-            return $this->error->error(mensaje: 'Error al obtener id',data:  $row_);
-        }
-
-        $select = (new dp_pais_html(html: $html))->select_dp_pais_id(cols: 6, con_registros:true,
-            id_selected:$row_->dp_pais_id,link: $link);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+            return $this->error->error(mensaje: 'Error al generar select',data:  $data);
 
         }
 
-        $data = new stdClass();
-        $data->row = $row_;
-        $data->select = $select;
         return $data;
     }
 
