debb9e90a3bc7b5dcc25541d22f7c11d85f4e077
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-05 10:22:33 -0500

    v0.85.8 Se simplifica funcion select

--- templates/directivas/selects.php
+++ templates/directivas/selects.php
@@ -3,6 +3,7 @@ namespace html;
 
 use gamboamartin\errores\errores;
 
+use gamboamartin\system\html_controler;
 use gamboamartin\system\init;
 use gamboamartin\template\html;
 use models\dp_calle;
@@ -16,6 +17,65 @@ class selects {
         $this->error = new errores();
     }
 
+    private function genera_obj_html(html $html, string $tabla): html_controler|array
+    {
+        $name_obj = $this->name_obk_html(tabla: $tabla);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener nombre de obj',data:  $name_obj);
+        }
+
+
+        $obj_html = $this->obj_html(name_obj: $name_obj,html: $html);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar objeto html',data:  $obj_html);
+        }
+        return $obj_html;
+    }
+
+    private function genera_select(PDO $link, html_controler $obj_html, stdClass $row_, string $tabla): array|string
+    {
+        $key_id = $this->key_id(tabla: $tabla);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar key id',data:  $key_id);
+        }
+
+        $name_function = $this->name_function(key_id: $key_id);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar name function',data:  $name_function);
+        }
+
+        $select = $obj_html->$name_function(cols: 6, con_registros:true, id_selected:$row_->$key_id,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+
+        }
+        return $select;
+    }
+
+    private function key_id(string $tabla): string
+    {
+        return $tabla.'_id';
+    }
+
+    private function name_function(string $key_id): string
+    {
+        return 'select_'.$key_id;
+    }
+
+    private function name_obk_html(string $tabla): string
+    {
+        return "html\\".$tabla.'_html';
+    }
+
+    private function obj_html(string $name_obj, html $html): html_controler
+    {
+        /**
+         * @var $obj_html html_controler
+         */
+        $obj_html = new $name_obj(html: $html);
+        return $obj_html;
+    }
+
     private function select_base(html $html, PDO $link, stdClass $row, string $tabla): array|stdClass
     {
         $row_ = $row;
@@ -25,13 +85,13 @@ class selects {
             return $this->error->error(mensaje: 'Error al obtener id',data:  $row_);
         }
 
-        $name_obj = "html\\".$tabla.'_html';
-        $obj_html = new $name_obj(html: $html);
-        $key_id = $tabla.'_id';
-        $name_function = 'select_'.$key_id;
 
+        $obj_html = $this->genera_obj_html(html: $html,tabla: $tabla);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar objeto html',data:  $obj_html);
+        }
 
-        $select = $obj_html->$name_function(cols: 6, con_registros:true, id_selected:$row_->$key_id,link: $link);
+        $select = $this->genera_select(link: $link,obj_html: $obj_html,row_: $row_,tabla: $tabla);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select',data:  $select);
 
@@ -40,6 +100,25 @@ class selects {
         $data = new stdClass();
         $data->row = $row_;
         $data->select = $select;
+        return $data;
+    }
+
+    /**
+     * Genera un select de tipo estado inicializado
+     * @param html $html Clade de template
+     * @param PDO $link conexion a bd
+     * @param stdClass $row Registro en operacion
+     * @return array|stdClass
+     */
+    public function dp_cp_id(html $html, PDO $link, stdClass $row): array|stdClass
+    {
+
+        $data = $this->select_base(html: $html,link:  $link, row: $row,tabla:  'dp_cp');
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $data);
+
+        }
+
         return $data;
     }
 
