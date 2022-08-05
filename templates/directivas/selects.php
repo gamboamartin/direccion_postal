53a7d216424467e35c075c1dd7bfbac0336e1ed0
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-05 09:52:22 -0500

    v0.82.8 Se municipio

--- templates/directivas/selects.php
+++ templates/directivas/selects.php
@@ -46,6 +46,35 @@ class selects {
     }
 
     /**
+     * Genera un select de tipo estado inicializado
+     * @param html $html Clade de template
+     * @param PDO $link conexion a bd
+     * @param stdClass $row Registro en operacion
+     * @return array|stdClass
+     */
+    public function dp_municipio_id(html $html, PDO $link, stdClass $row): array|stdClass
+    {
+        $row_ = $row;
+
+        $row_ = (new init())->row_value_id($row_, tabla: 'dp_municipio');
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener id',data:  $row_);
+        }
+
+        $select = (new dp_municipio_html(html: $html))->select_dp_municipio_id(cols: 6, con_registros:true,
+            id_selected:$row_->dp_estado_id,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+
+        }
+
+        $data = new stdClass();
+        $data->row = $row_;
+        $data->select = $select;
+        return $data;
+    }
+
+    /**
      * Genera un select de tipo pais inicializado
      * @param html $html Clade de template
      * @param PDO $link conexion a bd
