bb4aa5ae147693133e1357543c9986ba919c2ad1
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-05 09:51:04 -0500

    v0.81.8 Se simplifican funciones de selects

--- templates/directivas/selects.php
+++ templates/directivas/selects.php
@@ -16,46 +16,60 @@ class selects {
         $this->error = new errores();
     }
 
+    /**
+     * Genera un select de tipo estado inicializado
+     * @param html $html Clade de template
+     * @param PDO $link conexion a bd
+     * @param stdClass $row Registro en operacion
+     * @return array|stdClass
+     */
     public function dp_estado_id(html $html, PDO $link, stdClass $row): array|stdClass
     {
+        $row_ = $row;
 
-
-        $row = (new init())->row_value_id($row, tabla: 'dp_estado');
+        $row_ = (new init())->row_value_id($row_, tabla: 'dp_estado');
         if(errores::$error){
-            return $this->error->error(mensaje: 'Error al obtener id',data:  $row);
+            return $this->error->error(mensaje: 'Error al obtener id',data:  $row_);
         }
 
         $select = (new dp_estado_html(html: $html))->select_dp_estado_id(cols: 6, con_registros:true,
-            id_selected:$row->dp_estado_id,link: $link);
+            id_selected:$row_->dp_estado_id,link: $link);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select',data:  $select);
 
         }
 
         $data = new stdClass();
-        $data->row = $row;
+        $data->row = $row_;
         $data->select = $select;
         return $data;
     }
 
+    /**
+     * Genera un select de tipo pais inicializado
+     * @param html $html Clade de template
+     * @param PDO $link conexion a bd
+     * @param stdClass $row Registro en operacion
+     * @return array|stdClass
+     */
     public function dp_pais_id(html $html, PDO $link, stdClass $row): array|stdClass
     {
 
-
-        $row = (new init())->row_value_id($row, tabla: 'dp_pais');
+        $row_ = $row;
+        $row_ = (new init())->row_value_id($row_, tabla: 'dp_pais');
         if(errores::$error){
-            return $this->error->error(mensaje: 'Error al obtener id',data:  $row);
+            return $this->error->error(mensaje: 'Error al obtener id',data:  $row_);
         }
 
         $select = (new dp_pais_html(html: $html))->select_dp_pais_id(cols: 6, con_registros:true,
-            id_selected:$row->dp_pais_id,link: $link);
+            id_selected:$row_->dp_pais_id,link: $link);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select',data:  $select);
 
         }
 
         $data = new stdClass();
-        $data->row = $row;
+        $data->row = $row_;
         $data->select = $select;
         return $data;
     }
