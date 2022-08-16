9dc715a3f61e4be2b16be63e3bb97470a9b8c520
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-16 09:51:06 -0500

    v0.121.8 Se integran foraneas a calle pertenece

--- orm/dp_calle_pertenece.php
+++ orm/dp_calle_pertenece.php
@@ -8,7 +8,8 @@ use stdClass;
 class dp_calle_pertenece extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla=>false,'dp_colonia_postal'=>$tabla,'dp_calle'=>$tabla,'dp_cp'=>'dp_colonia_postal',
+            'dp_colonia'=>'dp_colonia_postal','dp_municipio'=>'dp_cp','dp_estado'=>'dp_municipio','dp_pais'=>'dp_estado');
         $campos_obligatorios[] = 'descripcion';
         $campos_obligatorios[] = 'descripcion_select';
 
