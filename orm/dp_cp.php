2c47cb16227be1ced990a4936e6a5b645726f075
mgamboa <gamboa.martin@gmail.com>
2022-10-10 11:19:46 -0500

    v0.146.10 Se integra cod 6 dig

--- orm/dp_cp.php
+++ orm/dp_cp.php
@@ -12,8 +12,8 @@ class dp_cp extends modelo{
         $campos_obligatorios[] = 'descripcion';
         $campos_obligatorios[] = 'descripcion_select';
 
-        $tipo_campos['codigo'] = 'cod_int_0_numbers';
-        $tipo_campos['descripcion'] = 'cod_int_0_numbers';
+        $tipo_campos['codigo'] = 'cod_int_0_5_numbers';
+        $tipo_campos['descripcion'] = 'cod_int_0_5_numbers';
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas, tipo_campos: $tipo_campos);
