d1b3292e0bb5d1a85dddd2254f43e13f9871bc5c
mgamboa <gamboa.martin@gmail.com>
2022-12-14 17:10:40 -0600

    v3.3.0 Se integra init pred

--- orm/dp_calle_pertenece.php
+++ orm/dp_calle_pertenece.php
@@ -168,6 +168,22 @@ class dp_calle_pertenece extends _base {
 
     private function integra_dp_calle_id_predeterminado(array $registro): array
     {
+        $existe = (new dp_calle(link: $this->link))->existe_predeterminado();
+        if(errores::$error){
+            return $this->error->error(
+                mensaje: 'Error al validar si existe predeterminado',data:  $existe);
+        }
+
+        if(!$existe){
+            $dp_calle_pred['predeterminado'] = 'activo';
+            $dp_calle_pred['codigo'] = 'PRED';
+            $r_dp_calle_pred = (new dp_calle(link: $this->link))->alta_registro(registro: $dp_calle_pred);
+            if(errores::$error){
+                return $this->error->error(
+                    mensaje: 'Error al insertar prederminado',data:  $r_dp_calle_pred);
+            }
+        }
+
         $dp_calle_id = (new dp_calle($this->link))->id_predeterminado();
         if(errores::$error){
             return $this->error->error(
