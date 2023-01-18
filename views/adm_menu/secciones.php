2c9f59bc3e51dfdbd3d261511cc54a694d6baee0
mgamboa <gamboa.martin@gmail.com>
2023-01-18 17:16:25 -0600

    v8.5.0 Se integra namespace in dp

--- views/adm_menu/secciones.php
+++ views/adm_menu/secciones.php
@@ -1,3 +1,3 @@
 <?php use config\generales;
 
-include (new generales())->path_base.'vendor/gamboa.martin/acl/views/adm_menu/secciones.php'; 
+include (new generales())->path_base.'vendor/gamboa.martin/acl/views/adm_menu/secciones.php';
