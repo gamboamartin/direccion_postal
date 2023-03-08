e1638f3197af254a410ccc9eaf3211d741b0744d
mgamboa <gamboa.martin@gmail.com>
2023-03-08 10:48:32 -0600

    v9.115.3 Se integra init pais

--- orm/dp_pais.php
+++ orm/dp_pais.php
@@ -1,6 +1,8 @@
 <?php
 namespace gamboamartin\direccion_postal\models;
+use base\orm\_defaults;
 use base\orm\_modelo_parent;
+use gamboamartin\errores\errores;
 use PDO;
 
 class dp_pais extends _modelo_parent {
@@ -17,6 +19,20 @@ class dp_pais extends _modelo_parent {
             columnas: $columnas, campos_view: $campos_view);
         $this->NAMESPACE = __NAMESPACE__;
         $this->etiqueta = 'Pais';
+
+        if(!isset($_SESSION['init'][$tabla])) {
+            $catalago = array();
+            $catalago[] = array('codigo' => 'MEX', 'descripcion' => 'MEXICO');
+
+
+            $r_alta_bd = (new _defaults())->alta_defaults(catalago: $catalago, entidad: $this);
+            if (errores::$error) {
+                $error = $this->error->error(mensaje: 'Error al insertar', data: $r_alta_bd);
+                print_r($error);
+                exit;
+            }
+            $_SESSION['init'][$tabla] = true;
+        }
     }
 
 }
