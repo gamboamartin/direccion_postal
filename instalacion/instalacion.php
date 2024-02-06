f22d209ec6c8cb92564433a1a2c28b61c3fc8a69
mgamboa <gamboa.martin@gmail.com>
2024-02-06 14:34:42 -0600

    v20.6.0 Se ajusta inst dp col

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -2,6 +2,9 @@
 namespace gamboamartin\direccion_postal\instalacion;
 
 use gamboamartin\administrador\models\_instalacion;
+use gamboamartin\direccion_postal\models\dp_colonia;
+use gamboamartin\direccion_postal\models\dp_colonia_postal;
+use gamboamartin\direccion_postal\models\dp_cp;
 use gamboamartin\direccion_postal\models\dp_estado;
 use gamboamartin\direccion_postal\models\dp_municipio;
 use gamboamartin\direccion_postal\models\dp_pais;
@@ -12,6 +15,89 @@ use stdClass;
 class instalacion
 {
 
+    private function _add_dp_colonia(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $create = (new _instalacion(link: $link))->create_table_new(table: 'dp_colonia');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al create table', data:  $create);
+        }
+        $out->create = $create;
+
+        $campos = new stdClass();
+        $campos->georeferencia = new stdClass();
+
+        $result = (new _instalacion(link: $link))->add_columns(campos: $campos,table:  'dp_colonia');
+
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar campos', data:  $result);
+        }
+        $out->columnas = $result;
+
+
+        return $out;
+
+    }
+
+    private function _add_dp_colonia_postal(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $create = (new _instalacion(link: $link))->create_table_new(table: 'dp_colonia_postal');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al create table', data:  $create);
+        }
+        $out->create = $create;
+
+        $foraneas = array();
+        $foraneas['dp_cp_id'] = new stdClass();
+        $foraneas['dp_colonia_id'] = new stdClass();
+
+        $foraneas_r = (new _instalacion(link:$link))->foraneas(foraneas: $foraneas,table:  'dp_colonia_postal');
+
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $foraneas_r);
+        }
+        $out->foraneas_r = $foraneas_r;
+
+
+
+        return $out;
+
+    }
+
+    private function _add_dp_cp(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $create = (new _instalacion(link: $link))->create_table_new(table: 'dp_cp');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al create table', data:  $create);
+        }
+        $out->create = $create;
+        $foraneas = array();
+        $foraneas['dp_municipio_id'] = new stdClass();
+
+        $foraneas_r = (new _instalacion(link:$link))->foraneas(foraneas: $foraneas,table:  'dp_cp');
+
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $foraneas_r);
+        }
+        $out->foraneas_r = $foraneas_r;
+
+        $campos = new stdClass();
+        $campos->georeferencia = new stdClass();
+
+        $result = (new _instalacion(link: $link))->add_columns(campos: $campos,table:  'dp_cp');
+
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar campos', data:  $result);
+        }
+        $out->columnas = $result;
+
+
+
+        return $out;
+
+    }
 
     private function _add_dp_estado(PDO $link): array|stdClass
     {
@@ -111,6 +197,105 @@ class instalacion
 
     }
 
+    private function dp_colonia(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $create = $this->_add_dp_colonia(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar create', data:  $create);
+        }
+
+        $dp_colonias_ins = array();
+        $dp_colonia_ins['id'] = '49728';
+        $dp_colonia_ins['codigo'] = '49728';
+        $dp_colonia_ins['descripcion_select'] = 'Residencial Revolución';
+        $dp_colonia_ins['descripcion'] = 'Residencial Revolución';
+        $dp_colonia_ins['georeferencia'] = 'SG';
+
+        $dp_colonias_ins[0] = $dp_colonia_ins;
+        foreach ($dp_colonias_ins as $dp_colonia_ins){
+            $alta = (new dp_colonia(link: $link))->inserta_registro_si_no_existe(registro: $dp_colonia_ins);
+            if(errores::$error){
+                return (new errores())->error(mensaje: 'Error al insertar',data:  $alta);
+            }
+            $out->altas[] = $alta;
+
+        }
+
+        return $out;
+
+    }
+
+    private function dp_colonia_postal(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $create = $this->_add_dp_colonia_postal(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar create', data:  $create);
+        }
+
+        $dp_colonias_postales_ins = array();
+        $dp_colonia_postal_ins['id'] = '23';
+        $dp_colonia_postal_ins['codigo'] = '45580 Residencial Revolución';
+        $dp_colonia_postal_ins['descripcion_select'] = '45580 Residencial Revolución Residencial Revolución - 45580';
+        $dp_colonia_postal_ins['descripcion'] = 'Residencial Revolución - 45580';
+        $dp_colonia_postal_ins['dp_cp_id'] = '2';
+        $dp_colonia_postal_ins['dp_colonia_id'] = '49728';
+
+        $dp_colonias_postales_ins[0] = $dp_colonia_postal_ins;
+        foreach ($dp_colonias_postales_ins as $dp_colonia_postal_ins){
+            $alta = (new dp_colonia_postal(link: $link))->inserta_registro_si_no_existe(registro: $dp_colonia_postal_ins);
+            if(errores::$error){
+                return (new errores())->error(mensaje: 'Error al insertar',data:  $alta);
+            }
+            $out->altas[] = $alta;
+
+        }
+
+        return $out;
+
+    }
+
+    private function dp_cp(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $create = $this->_add_dp_cp(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar create', data:  $create);
+        }
+
+        $dp_cps_ins = array();
+        $dp_cp_ins['id'] = '1';
+        $dp_cp_ins['codigo'] = '45010';
+        $dp_cp_ins['descripcion_select'] = '45010 45010';
+        $dp_cp_ins['descripcion'] = '45010';
+        $dp_cp_ins['dp_municipio_id'] = '1805';
+        $dp_cp_ins['georeferencia'] = 'SG';
+        $dp_cps_ins[0] = $dp_cp_ins;
+
+
+        $dp_cp_ins['id'] = '2';
+        $dp_cp_ins['codigo'] = '45580';
+        $dp_cp_ins['descripcion_select'] = '45580 45580';
+        $dp_cp_ins['descripcion'] = '45580';
+        $dp_cp_ins['dp_municipio_id'] = '1649';
+        $dp_cp_ins['georeferencia'] = 'SG';
+
+        $dp_cps_ins[1] = $dp_cp_ins;
+
+        foreach ($dp_cps_ins as $dp_cp_ins){
+            $alta = (new dp_cp(link: $link))->inserta_registro_si_no_existe(registro: $dp_cp_ins);
+            if(errores::$error){
+                return (new errores())->error(mensaje: 'Error al insertar',data:  $alta);
+            }
+            $out->altas[] = $alta;
+
+        }
+
+        return $out;
+
+    }
+
     private function dp_estado(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -149,13 +334,30 @@ class instalacion
         }
 
         $dp_municipios_ins = array();
+
         $dp_municipio_ins['id'] = '230';
         $dp_municipio_ins['codigo'] = 'JAL008';
         $dp_municipio_ins['descripcion_select'] = 'JAL008 Arandas';
         $dp_municipio_ins['descripcion'] = 'Arandas';
         $dp_municipio_ins['dp_estado_id'] = '14';
-
         $dp_municipios_ins[0] = $dp_municipio_ins;
+
+
+        $dp_municipio_ins['id'] = '1805';
+        $dp_municipio_ins['codigo'] = 'JAL120';
+        $dp_municipio_ins['descripcion_select'] = 'JAL120 Zapopan';
+        $dp_municipio_ins['descripcion'] = 'Zapopan';
+        $dp_municipio_ins['dp_estado_id'] = '14';
+        $dp_municipios_ins[1] = $dp_municipio_ins;
+
+        $dp_municipio_ins['id'] = '1649';
+        $dp_municipio_ins['codigo'] = 'JAL098';
+        $dp_municipio_ins['descripcion_select'] = 'JAL098 San Pedro Tlaquepaque';
+        $dp_municipio_ins['descripcion'] = 'San Pedro Tlaquepaque';
+        $dp_municipio_ins['dp_estado_id'] = '14';
+        $dp_municipios_ins[2] = $dp_municipio_ins;
+
+
         foreach ($dp_municipios_ins as $dp_municipio_ins){
             $alta = (new dp_municipio(link: $link))->inserta_registro_si_no_existe(registro: $dp_municipio_ins);
             if(errores::$error){
@@ -168,8 +370,6 @@ class instalacion
         return $out;
 
     }
-
-
 
     private function dp_pais(PDO $link): array|stdClass
     {
@@ -199,12 +399,16 @@ class instalacion
 
     }
 
-
     final public function instala(PDO $link): array|stdClass
     {
 
         $result = new stdClass();
 
+        $dp_colonia = $this->dp_colonia(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar dp_colonia', data:  $dp_colonia);
+        }
+        $result->dp_colonia = $dp_colonia;
 
         $dp_pais = $this->dp_pais(link: $link);
         if(errores::$error){
@@ -223,6 +427,18 @@ class instalacion
             return (new errores())->error(mensaje: 'Error al ajustar dp_municipio', data:  $dp_municipio);
         }
         $result->dp_municipio = $dp_municipio;
+
+        $dp_cp = $this->dp_cp(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar dp_cp', data:  $dp_cp);
+        }
+        $result->dp_cp = $dp_cp;
+
+        $dp_colonia_postal = $this->dp_colonia_postal(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar dp_colonia_postal', data:  $dp_colonia_postal);
+        }
+        $result->dp_colonia_postal = $dp_colonia_postal;
 
 
         return $result;
