2c6c1e3b716adc47562aa686508227218092ccc9
mgamboa <gamboa.martin@gmail.com>
2024-03-06 09:50:38 -0600

    v21.0.0 Se integra v20 template in dp

--- tests/controllers/controlador_dp_calleTest.php
+++ tests/controllers/controlador_dp_calleTest.php
@@ -61,6 +61,49 @@ class controlador_dp_calleTest extends test {
         errores::$error = false;
     }
 
+    public function test_inicializa_priedades(): void
+    {
+        errores::$error = false;
+        $_GET['session_id'] = 1;
+        $_GET['seccion'] = 'dp_calle';
+        $_GET['accion'] = 'get_calle';
+        $_SESSION['grupo_id'] = '2';
+        $_SESSION['usuario_id'] = '2';
+
+        $del = (new base_test())->del_adm_seccion(link: $this->link);
+        if(errores::$error){
+            $error = (new errores())->error(mensaje: 'Error al del',data:  $del);
+            print_r($error);
+            exit;
+        }
+
+        $adm_accion_id = mt_rand(10000000,99999999);
+        $adm_seccion_id = mt_rand(10000000,99999999);
+        $alta = (new base_test())->alta_adm_accion(link: $this->link, adm_seccion_descripcion: 'dp_calle',
+            adm_seccion_id: $adm_seccion_id, descripcion: 'get_calle', id: $adm_accion_id);
+        if(errores::$error){
+            $error = (new errores())->error(mensaje: 'Error al insertar',data:  $alta);
+            print_r($error);
+            exit;
+        }
+
+        $ctl = new controlador_dp_calle(link: $this->link,paths_conf: $this->paths_conf);
+
+
+        $resultado = $ctl->inicializa_priedades();
+
+
+        $this->assertIsArray($resultado);
+        $this->assertNotTrue(errores::$error);
+        $this->assertEquals("Código",$resultado['codigo']->place_holder);
+        $this->assertEquals(4,$resultado['codigo']->cols);
+
+        $this->assertEquals("Calle",$resultado['descripcion']->place_holder);
+        $this->assertEquals(12,$resultado['descripcion']->cols);
+
+        errores::$error = false;
+    }
+
 
 
 
